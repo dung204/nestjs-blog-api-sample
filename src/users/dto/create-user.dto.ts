f065ac7864816cd2323c27dc37f4d9da89d45c50
dung204 <acezombiev4@gmail.com>
2024-02-11 23:26:46 +0700

    feat(users): apply validation decorators
    refactor: add non-null assertions to env values
    
    chore: add `class-transformer` and `class-validator` dependencies
    
    refactor: add global validation pipe to app
    
    Merge branch 'main' to 'feat/users'

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,30 @@
+import { IsName } from '@/common/decorators/is-name.decorator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  IsUrl,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
   email!: string;
+
+  @IsStrongPassword()
   password!: string;
+
+  @IsName()
   firstName!: string;
+
+  @IsName()
   lastName!: string;
+
+  @IsOptional()
+  @IsString()
   address?: string;
+
+  @IsOptional()
+  @IsUrl()
   avatarUrl?: string;
 }
