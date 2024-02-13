137189d96d307d7e644224d3cad5181495584da1
dung204 <acezombiev4@gmail.com>
2024-02-13 20:34:48 +0700

    docs(users): implement swagger decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,39 +7,87 @@ import {
   Param,
   Delete,
   Query,
-  HttpCode,
 } from '@nestjs/common';
 import { UsersService } from '@/users/users.service';
 import { CreateUserDto } from '@/users/dto/create-user.dto';
 import { UpdateUserDto } from '@/users/dto/update-user.dto';
 import PaginationOptionsDto from '@/common/dto/pagination-options.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from '@/users/entities/user.entity';
+import { ApiOkPaginatedResponse } from '@/common/decorators/api-ok-paginated-response.decorator';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiCreatedResponse({
+    description: 'The user has been successfully created.',
+    type: User,
+  })
+  @ApiBadRequestResponse({ description: 'Bad request.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiOkPaginatedResponse(User)
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   findAll(@Query() paginationOptionsDto: PaginationOptionsDto) {
     return this.usersService.findAll(paginationOptionsDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a user by ID' })
+  @ApiParam({ name: 'id', description: 'The user ID' })
+  @ApiOkResponse({
+    description: 'The user has been successfully retrieved.',
+    type: User,
+  })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a user by ID' })
+  @ApiParam({ name: 'id', description: 'The user ID' })
+  @ApiBody({ type: UpdateUserDto, required: false })
+  @ApiOkResponse({
+    description: 'The user has been successfully updated.',
+    type: User,
+  })
+  @ApiBadRequestResponse({ description: 'Bad request.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  @HttpCode(204)
+  @ApiOperation({ summary: 'Delete a user by ID' })
+  @ApiParam({ name: 'id', description: 'The user ID' })
+  @ApiNoContentResponse({
+    description: 'The user has been successfully deleted.',
+  })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
