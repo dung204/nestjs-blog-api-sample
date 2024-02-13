55ebeb90c091fcc3924ad37e54c3f9d56da23747
dung204 <acezombiev4@gmail.com>
2024-02-13 16:13:26 +0700

    docs: implement swagger decorators for pagination dtos

--- src/common/dto/pagination-options.dto.ts
+++ src/common/dto/pagination-options.dto.ts
@@ -1,16 +1,30 @@
 import { Order } from '@/common/enums/Order';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
 import { IsEnum, IsInt } from 'class-validator';
 
 export default class PaginationOptionsDto {
+  @ApiProperty({
+    description: 'The current page number',
+    default: 1,
+  })
   @Type(() => Number)
   @IsInt()
   readonly page: number = 1;
 
+  @ApiProperty({
+    description: 'The number of items in a page',
+    default: 10,
+  })
   @Type(() => Number)
   @IsInt()
   readonly limit: number = 10;
 
+  @ApiProperty({
+    description: 'The order of the items',
+    enum: Order,
+    default: Order.ASC,
+  })
   @Transform(({ value }) => value.toUpperCase())
   @IsEnum(Order)
   readonly order: Order = Order.ASC;
--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,10 +1,19 @@
 import PaginationMetaDto from '@/common/dto/pagination-meta.dto';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray } from 'class-validator';
 
 export default class PaginationDto<T> {
+  @ApiProperty({
+    isArray: true,
+    description: 'An array of `T` data',
+  })
   @IsArray()
   readonly data: T[];
 
+  @ApiProperty({
+    description: 'Pagination metadata',
+    type: PaginationMetaDto,
+  })
   readonly meta: PaginationMetaDto;
 
   constructor(data: T[], meta: PaginationMetaDto) {
